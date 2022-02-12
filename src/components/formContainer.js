2c21a9a3bb98064948c3cedcab60b68eb5b9c8eb
maddy1381 <ms1381598@gmail.com>
2022-02-12 15:59:56 +0530

    Created the initital form

--- src/components/formContainer.js
+++ src/components/formContainer.js
@@ -1,18 +1,58 @@
 import React from "react";
 import "../assets/formContainer.css";
-import DateView from "react-datepicker";
+// import DateView from "react-datepicker";
+// import "react-datepicker/dist/react-datepicker.css";
+
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const FormContainer = () => {
   const [startDate, setStartDate] = React.useState(new Date());
+  //for time
+  const [startTime, setStartTime] = React.useState(new Date());
+  //for num of people
+  const [numOfPeople, setNumOfPeople] = React.useState(2);
+
   return (
     <div className="form-container">
-      <div className="date-picker">
-        <DateView
-          selected={startDate}
-          onChange={(date) => setStartDate(date)}
-          
-        />
+      <div className="form-container-inputs">
+        <div className="date-picker form-input">
+          <label>Select a Date</label>
+          <DatePicker
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            dateFormat="dd/MM/yyyy"
+            minDate={new Date()}
+            className="date-component input-controller"
+          />
+        </div>
+        <div className="time-picker form-input">
+          <label>Select a Time</label>
+          <DatePicker
+            selected={startTime}
+            onChange={(date) => setStartTime(date)}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={30}
+            timeCaption="Time"
+            dateFormat="h:mm aa"
+            className="time-component input-controller"
+          />
+        </div>
+        <div className="num-of-people form-input">
+          <label>Number of People</label>
+          <input
+            type="number"
+            value={numOfPeople}
+            onChange={(e) => setNumOfPeople(e.target.value)}
+            className="input-controller"
+          />
+        </div>
+      </div>
+      <div className="btn-container">
+        <button type="submit" className="submit-btn">
+          Check for Availability
+        </button>
       </div>
     </div>
   );
